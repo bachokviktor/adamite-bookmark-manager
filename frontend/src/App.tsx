09110726b0a931bb3586c7e342aef3841b309f17
Bachok Viktor <bachok.viktor@gmail.com>
2026-03-23 19:40:25 +0200

    Implement login form.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router"
 import Home from "./components/Home"
 import Register from "./components/Register"
+import Login from "./components/Login"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 	<Routes>
 	  <Route path="/" element={<Home />} />
 	  <Route path="/register" element={<Register />} />
+	  <Route path="/login" element={<Login />} />
 	</Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from "react"
 import {useNavigate, Link} from "react-router"
 import api from "../api"
 
-function Register() {
+function Login() {
   const navigate = useNavigate()
 
   const [username, setUsername] = useState<string>("")
   const [usernameValid, setUsernameValid] = useState<boolean>(false)
   const [password, setPassword] = useState<string>("")
   const [passwordValid, setPasswordValid] = useState<boolean>(false)
-  const [matchPassword, setMatchPassword] = useState<string>("")
-  const [passwordMatching, setPasswordMatching] = useState<boolean>(false)
   const [errorMessage, setErrorMessage] = useState<string>("")
 
   useEffect(() => {
@@ -19,25 +17,23 @@ function Register() {
 
   useEffect(() => {
     setPasswordValid(password.trim().length >= 6)
-
-    setPasswordMatching(password === matchPassword)
-  }, [password, matchPassword])
+  }, [password])
 
   useEffect(() => {
     setErrorMessage("")
-  }, [username, password, matchPassword])
+  }, [username, password])
 
-  const handleRegister = async (e: React.SubmitEvent<HTMLFormElement>) => {
+  const handleLogin = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     try {
       const response = await api.post(
-	"users/",
+	"token/",
 	{username: username, password: password}
       )
 
       console.log(response.data)
-      navigate("/login")
+      navigate("/")
     } catch (error) {
       setErrorMessage("Something went wrong.")
     }
@@ -46,8 +42,8 @@ function Register() {
   return (
     <div className="form-wrapper">
       <div className="form-container">
-        <h2>Registration</h2>
-        <form onSubmit={handleRegister}>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
           <label htmlFor="usernameInput">Username {!usernameValid && (username.trim().length > 0) ? <span className="error-message">Invalid</span> : ""}</label><br/>
           <input
 	    name="usernameInput"
@@ -68,28 +64,18 @@ function Register() {
 	    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setPassword(e.target.value)}}
 	    value={password} /><br/>
 
-          <label htmlFor="matchPasswordInput">Confirm Password {!passwordMatching && (matchPassword.trim().length > 0) ? <span className="error-message">Doesn't match</span> : ""}</label><br/>
-          <input
-	    name="matchPasswordInput"
-	    id="matchPasswordInput"
-	    type="password"
-	    required
-	    placeholder="confirm password"
-	    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setMatchPassword(e.target.value)}}
-	    value={matchPassword} /><br/>
-
 	  {errorMessage && <p className="error-message">{errorMessage}</p>}
 
           <button
 	    className="b-success"
 	    type="submit"
-	    disabled={(usernameValid && passwordValid && passwordMatching) ? false : true}>Register</button>
+	    disabled={(usernameValid && passwordValid) ? false : true}>Login</button>
         </form>
 
-	<p>Already have an account?<br/><Link to="/login">Login</Link></p>
+	<p>Don't have an account?<br/><Link to="/register">Register</Link></p>
       </div>
     </div>
   )
 }
 
-export default Register
+export default Login
